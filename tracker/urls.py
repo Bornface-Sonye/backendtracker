2846749f5bdebe74ebccdf89361316133de872f5
Bornface-Sonye <bornfacesonye@gmail.com>
2025-04-10 20:55:13 -0700

    Result and Nominol Roll urls.py done, test pending

--- tracker/urls.py
+++ tracker/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from . import views
 from .views import (
-    SignUpView, LoginView, COD_DashboardView, Exam_DashboardView, Lecturer_DashboardView, LogoutView, StudentRegNo,
-    StudentSelectView, MissingMarkSelectView
+    SignUpView, LoginView, COD_DashboardView, Exam_DashboardView, Lecturer_DashboardView, LogoutView, StudentSelectView, 
+    MissingMarkSelectView, COD_NominalRollListView, COD_ResultListView, COD_LoadResultView, COD_LoadNominalRollView,
+    Exam_NominalRollListView, Exam_ResultListView, Exam_LoadResultView, Exam_LoadNominalRollView, NominalRollListView,
+    ResultListView, LoadResultView, LoadNominalRollView
+    
+    
 )
 
 urlpatterns = [
@@ -16,5 +20,23 @@ urlpatterns = [
     path('cod-dashboard/',COD_DashboardView.as_view(), name='cod-dashboard'),
     
     path('logout/', LogoutView.as_view(), name='logout'),
+    
+    path('cod/load-nominal-roll/', COD_LoadNominalRollView.as_view(), name='cod-load-nominal-roll'),
+    path('cod/load-result/', COD_LoadResultView.as_view(), name='cod-load-result'),
+
+    path('cod/nominal-roll/', COD_NominalRollListView.as_view(), name='cod-nominal-roll'),
+    path('cod/result/', COD_ResultListView.as_view(), name='cod-result'),
+    
+    path('exam/load-nominal-roll/', Exam_LoadNominalRollView.as_view(), name='exam-load-nominal-roll'),
+    path('exam/load-result/', Exam_LoadResultView.as_view(), name='exam-load-result'),
+    
+    path('exam/nominal-roll/', Exam_NominalRollListView.as_view(), name='exam-nominal-roll'),
+    path('exam/result/', Exam_ResultListView.as_view(), name='exam-result'),
+
+    path('load-nominal-roll/', LoadNominalRollView.as_view(), name='load-nominal-roll'),
+    path('load-result/', LoadResultView.as_view(), name='load-result'),
+
+    path('nominal-roll/', NominalRollListView.as_view(), name='nominal-roll'),
+    path('result/', ResultListView.as_view(), name='result'),
 
 ]
